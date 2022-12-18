7ace89c767bea844dd221f9159007018641471fd
Squair <georgesquair@gmail.com>
2022-12-18 11:08:57 +0000

    Update title; Remove boilerplate

--- react/src/components/Measurement.tsx
+++ react/src/components/Measurement.tsx
@@ -10,7 +10,7 @@ const Measurement: FunctionComponent<MeasurementProps> = ({ name, value, centerO
     return (
         <div style={{ display: 'flex', flexDirection: 'column', marginTop: centerOffset }}>
             <p style={{ textAlign: 'center' }}>{name}</p>
-            <h1 style={{ padding: 0, margin: 0 }}>{value}%</h1>
+            <h1 style={{ padding: 0, margin: 0 }}>{value}</h1>
         </div>
     )
 }
