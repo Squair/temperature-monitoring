b95b8813d2905ed9b6419eb67b5e6ff57deab4f4
Squair <georgesquair@gmail.com>
2022-12-19 16:34:31 +0000

    Extract socket server config into env vars; Stlying changes; Only emit event if socket has connection

--- python/temperature-monitoring.py
+++ python/temperature-monitoring.py
@@ -1,36 +1,49 @@
 import socketio
 import asyncio
+import uuid
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 sio = socketio.AsyncClient()
 
 @sio.event
 def connect():
-    print("I'm connected!")
+    print("Successfully connected to socket server.")
 
 @sio.event
 def connect_error(data):
-    print("The connection failed!")
+    print("Could not connect to socket server")
 
 @sio.event
 def disconnect():
-    print("I'm disconnected!")
+    print("Disconnected from socket server")
 
-async def Main():
-    await sio.connect('http://localhost:3001?deviceId=1&monitoringGroupId=1')
+def read_sensor_values(i):
+    temperatureRecording = {
+            'id': str(uuid.uuid4()),
+            'temperature': i,
+            'humidity': i,
+            'timeReceived': str(datetime.now())
+    }
+    return temperatureRecording
 
+async def main():
+    socketHost = os.environ.get("SOCKET_HOST")
+    socketPort = os.environ.get("SOCKET_PORT")
+
+    await sio.connect(f'http://{socketHost}:{socketPort}?deviceId=1&monitoringGroupId=1')
+    
     i = 0
     while(1):
         i += 1
-        temperatureRecording = {
-            'id': '1',
-            'temperature': i,
-            'humidity': '25',
-            'timeReceived': '2022-12-19'
-        }
-        await sio.emit('send-temperature-recording', temperatureRecording)
+
+        if (sio.connected):
+            await sio.emit('send-temperature-recording', read_sensor_values(i))
         await asyncio.sleep(3)
 
-asyncio.run(Main())  # main loop
+asyncio.run(main())  # main loop
 
 
 
