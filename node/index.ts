c34dbe26069765c0a094ee3c92c9894a65a5edcd
Squair <georgesquair@gmail.com>
2023-01-15 19:42:55 +0000

    Begin adding support for multiple devices; Each device now has it's own target temperature

--- node/index.ts
+++ node/index.ts
@@ -11,13 +11,17 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS;
 const port = process.env.PORT;
 
 const io = new Server(parseInt(port), { cors: { origin: "*" } });
-let targetTemperature = 18;
 
 type HeaterState = "Undiscovered" | "On" | "Off"
 
 let heaterState: HeaterState = "Undiscovered";
 
-const deviceMostRecentTemperature: { [key: string]: ITemperatureRecording } = {}
+const deviceMetadata: {
+    [key: string]: {
+        mostRecentRecording?: ITemperatureRecording;
+        targetTemperature?: number
+    }
+} = {}
 
 axios.defaults.headers.common = {
     "X-API-Key": process.env.KEY,
@@ -34,7 +38,9 @@ export interface IUserSettings {
     targetTemperature: number;
 }
 
-const updateHeaterState = async (recording: ITemperatureRecording) => {
+const updateHeaterState = async (recording: ITemperatureRecording, deviceId: string) => {
+    const targetTemperature = deviceMetadata[deviceId].targetTemperature;
+
     if (recording.temperature < targetTemperature && (heaterState == 'Off' || heaterState == 'Undiscovered')) {
         // call api to turn on heater
         const response = await axios.put(`${process.env.MEROSS_HOST}/heater?state=1`);
@@ -50,20 +56,19 @@ const updateHeaterState = async (recording: ITemperatureRecording) => {
     }
 }
 
-const handleTargetTemperatureChange = async (settings: IUserSettings, deviceId: string) => {
-    targetTemperature = settings.targetTemperature;
-    const recording = deviceMostRecentTemperature[deviceId];
+const handleTargetTemperatureChange = async (targetTemperature: number, deviceId: string) => {
+    targetTemperature = targetTemperature;
+    deviceMetadata[deviceId] = { ...deviceMetadata[deviceId], targetTemperature };
 
-    if (!recording) return;
-    
-    await updateHeaterState(recording);
+    if (!deviceMetadata[deviceId]) return;
+
+    await updateHeaterState(deviceMetadata[deviceId].mostRecentRecording, deviceId);
 }
 
 const handleSendTemperatureRecording = async (recording: ITemperatureRecording, socket: Socket, deviceId: string, monitoringGroupId: string) => {
     socket.to(monitoringGroupId).emit("recieve-temperature-recording", recording);
-    deviceMostRecentTemperature[deviceId] = recording;
-
-    await updateHeaterState(recording);
+    deviceMetadata[deviceId] = { ...deviceMetadata[deviceId], mostRecentRecording: recording };
+    await updateHeaterState(recording, deviceId);
 }
 
 io.on("connection", (socket) => {
@@ -73,8 +78,6 @@ io.on("connection", (socket) => {
     const monitoringGroupId = socket.handshake.query.monitoringGroupId as string;
 
     console.log("Incoming connection...");
-
-    deviceMostRecentTemperature[deviceId] = { id: "1", temperature: 12, humidity: 12, timeReceived: new Date() }
 
     if (deviceId) {
         console.log("Temperature node connected.");
@@ -87,7 +90,7 @@ io.on("connection", (socket) => {
         console.log("User connected.");
         socket.join(monitoringGroupId);
 
-        socket.on("target-temperature-change", handleTargetTemperatureChange);
+        socket.on("target-temperature-change", (settings) => handleTargetTemperatureChange(settings.targetTemperature, "1"));
     }
 
     // This will emulate events being recieved and broadcast, for testing purposes only.
