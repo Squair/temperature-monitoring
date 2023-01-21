77e693d72cca0a18277da193d91d2a5e681172a6
Squair <georgesquair@gmail.com>
2023-01-21 21:54:46 +0000

    Add trending temperature display depending on average temp is rising or falling

--- react/src/components/Measurement.tsx
+++ react/src/components/Measurement.tsx
@@ -1,16 +1,28 @@
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import { FunctionComponent } from "react";
 
 interface MeasurementProps {
     name: string;
     value: string;
     centerOffset: string;
+    isTrendingHigher?: boolean
+    alignTrend: 'left' | 'right'
 }
 
-const Measurement: FunctionComponent<MeasurementProps> = ({ name, value, centerOffset }) => {
+const Measurement: FunctionComponent<MeasurementProps> = ({ name, value, centerOffset, isTrendingHigher, alignTrend }) => {
+    const iconStlying = { height: '2em', width: '2em', alignSelf: 'flex-end' };
+
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', marginTop: centerOffset }}>
-            <p style={{ textAlign: 'center' }}>{name}</p>
-            <h1 style={{ padding: 0, margin: 0 }}>{value}</h1>
+        <div style={{ display: 'flex', flexDirection: alignTrend === 'left' ? 'row' : 'row-reverse', marginTop: centerOffset }}>
+            {isTrendingHigher
+                ? <KeyboardDoubleArrowUpIcon sx={{ ...iconStlying }} />
+                : <KeyboardDoubleArrowDownIcon sx={{ ...iconStlying }} />}
+
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <p style={{ textAlign: 'center' }}>{name}</p>
+                <h1 style={{ padding: 0, margin: 0 }}>{value}</h1>
+            </div>
         </div>
     )
 }
--- react/src/components/TemperatureDisplay.tsx
+++ react/src/components/TemperatureDisplay.tsx
@@ -1,27 +1,44 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { useTemperatureUtilities } from "../hooks/useTemperatureUtilities";
 import { ITemperatureRecording } from "../interface/ITemperatureRecording";
 import { Unit } from "../type/Unit";
 import Measurement from "./Measurement";
 
 interface TemperatureDisplayProps {
-    currentTemperature: ITemperatureRecording;
+    temperatures: ITemperatureRecording[];
     unit: Unit;
 }
 
-const TemperatureDisplay: FunctionComponent<TemperatureDisplayProps> = ({ currentTemperature, unit }) => {
+const TemperatureDisplay: FunctionComponent<TemperatureDisplayProps> = ({ temperatures, unit }) => {
+    const [isTrendingHigher, setIsTrendingHigher] = useState<{ temperature: boolean, humidity: boolean }>();
     const { getTemperatureUnitSymbol } = useTemperatureUtilities();
+
+    const currentTemperature = temperatures[0];
 
     const temperatureDisplay = `${currentTemperature.temperature.toFixed(2).toString()}${getTemperatureUnitSymbol(unit)}`;
     const humidityDisplay = `${currentTemperature.humidity.toFixed(2).toString()}%`;
-    
+
+    const averageTemperatures = (temperatures: ITemperatureRecording[]) => temperatures.map(t => t.temperature).reduce((x, i) => x + i, 0) / temperatures.length;
+    const averageHumidities = (temperatures: ITemperatureRecording[]) => temperatures.map(t => t.humidity).reduce((x, i) => x + i, 0) / temperatures.length;
+
+    useEffect(() => {
+        const isTemperatureTrendingHigher = currentTemperature.temperature >= averageTemperatures(temperatures);
+        const isHumidityTrendingHigher = currentTemperature.temperature >= averageHumidities(temperatures);
+
+        if (temperatures.length == 1 || temperatures.length % 3 === 0) {
+            setIsTrendingHigher({ temperature: isTemperatureTrendingHigher, humidity: isHumidityTrendingHigher });
+        }
+
+    }, [temperatures]);
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-            <Measurement name='Temperature' value={temperatureDisplay} centerOffset='-10em' />
+            
+            <Measurement name='Temperature' value={temperatureDisplay} centerOffset='-10em' isTrendingHigher={isTrendingHigher?.temperature} alignTrend='left' />
 
             <div style={{ height: '105%', width: '0.25em', borderRadius: '1em', backgroundColor: 'white', transform: 'rotate(20deg)' }} />
 
-            <Measurement name='Humidity' value={humidityDisplay} centerOffset='10em' />
+            <Measurement name='Humidity' value={humidityDisplay} centerOffset='10em' isTrendingHigher={isTrendingHigher?.humidity} alignTrend='right' />
         </div>
     )
 }
