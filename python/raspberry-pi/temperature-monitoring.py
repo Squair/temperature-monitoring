0d2c386f5eb1c36840f9131d06be203afc110484
Squair <georgesquair@gmail.com>
2024-06-17 18:38:38 +0100

    Build in manual socket io retries to fix long term connected false positives; Remove hacky i2c bus reset; Remove decorated event listeners;

--- python/raspberry-pi/temperature-monitoring.py
+++ python/raspberry-pi/temperature-monitoring.py
@@ -7,55 +7,23 @@ from dotenv import load_dotenv
 import os
 import smbus
 import RPi.GPIO as GPIO
-import requests
 
 load_dotenv()
-sio = socketio.AsyncClient()
+sio = socketio.AsyncClient(False)
 i2c_address = 0x44
 i2c_clock_pin = 0x03
 renew_socket_connection_timeout_in_seconds = 300
 
-@sio.event
-def connect():
-    print("Successfully connected to socket server.")
-
-@sio.event
-def connect_error(data):
-    print("Could not connect to socket server")
-
-@sio.event
-def disconnect():
-    print("Disconnected from socket server")
-
-def reset_i2c_bus():
-    # ideas: 
-    # restart i2c drivers / modules
-    # cycle power with another smart switch (bad)
-    # try clock bit banging approach further
-    # try a different lib other than smbus
-    # write reset command to i2c address
-
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(5, GPIO.OUT)
-    # GPIO.output(5, GPIO.HIGH)
-
-    # for i in range(10):
-    #     time.sleep(0.35 / 1000000.0)  
-    #     GPIO.output(5, GPIO.LOW)
-    #     time.sleep(0.35 / 1000000.0)  
-    #     GPIO.output(5, GPIO.HIGH)
-    
-    # Yes this is gross, but only way I've found to potentially recover i2c bus
-    requests.put(f'{os.environ.get("MEROSS_FLASK_HOST")}/raspberry-pi/reset', headers={'X-API-KEY': os.environ.get("MEROSS_FLASK_API_KEY")})
 
 def read_sensor_values():
     bus = smbus.SMBus(1)
     
-    # SHT31 address, 0x44(68)
+    # We have to first write to the address before we can read it
+    # Otherwise it will hold GPIO 3 low and causes timeouts on the i2c scan
     bus.write_i2c_block_data(i2c_address, 0x2C, [0x06])
 
     time.sleep(0.5)
-    # SHT31 address, 0x44(68)
+    
     # Read data back from 0x00(00), 6 bytes
     # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
     data = bus.read_i2c_block_data(i2c_address, 0x00, 6)
@@ -77,37 +45,61 @@ def read_sensor_values():
     }
     return temperatureRecording
 
-async def main():
+async def connect_to_socket_server_with_retry():
     socketHost = os.environ.get("SOCKET_HOST")
+    connectTimeoutInSeconds = 10
+    connected = False
+    
+    while(not connected):
+        print(f"{str(datetime.now())} - Attempting to connect to socket server...") 
+
+        timeoutTask = asyncio.create_task(asyncio.sleep(connectTimeoutInSeconds))
+        socketConnectTask = asyncio.create_task(sio.connect(f'{socketHost}?deviceId=1&monitoringGroupId=1'))
+
+        # Rarely its been seen that attempts to connect to socket server get stuck, despite it having an inbuilt retry mechanism
+        # This will prevent that by returning after connectTimeoutInSeconds if the connect task hasnt finished.
+        done, pending = await asyncio.wait(
+            [timeoutTask, socketConnectTask], 
+            return_when=asyncio.FIRST_COMPLETED
+        )
+        
+        # cancel the other tasks
+        for task in pending:
+            task.cancel()
+
+        for task in done:
+            if (task is socketConnectTask):
+                connected = True
+                return
+            elif(task is timeoutTask):
+                print(f"{str(datetime.now())} - Timeout task reached after {connectTimeoutInSeconds} seconds while trying to connect to socket server, will retry...")
+
+async def main(sio):
     timeLastConnected = datetime.now()
 
     while(1):
-        try:
-            await asyncio.sleep(3)
-
-            # Try read temperature first, i2c bus might be stuck and causing chaos, so this will at least cause a reset if it throws
-            temperature_recording = read_sensor_values()
+        try:      
 
             # After long periods of running, socket reports connected although server does not receive emitted events.
             # This will simply force closing and then re-open a new connection after a certain time has elapsed.
             if(sio.connected and (datetime.now() - timeLastConnected).total_seconds() >= renew_socket_connection_timeout_in_seconds):
                 print(f"{str(datetime.now())} - Renew socket timeout reached, disconnecting from socket server")
-                await sio.disconnect()
-
+                await sio.disconnect()      
             if (not sio.connected):
-                print(f"{str(datetime.now())} - Attempting to connect to socket server...")
-                await sio.connect(f'{socketHost}?deviceId=1&monitoringGroupId=1')
+                await connect_to_socket_server_with_retry()
                 timeLastConnected = datetime.now()
 
             if (sio.connected):
+                print(f"{str(datetime.now())} - Sending recording to socket server...")
+                temperature_recording = read_sensor_values()
                 await sio.emit('send-temperature-recording', temperature_recording)
-
-        except IOError:
-            # I2c bus will start timing out as clock pin can get stuck low, 
-            print(f"{str(datetime.now())} - I/O error, likely i2c bus is dead, attempting to reset...")
-            reset_i2c_bus()
-            print(f"{str(datetime.now())} - Reset complete")
+                
+                # Sleep in between readings
+                await sio.sleep(3)
+            
+        except IOError as ie:
+            print(f"{str(datetime.now())} - I/O error caught: {str(ie)}")
         except Exception as e:
             print(f"{str(datetime.now())} - Exception caught: {str(e)}")
 
-asyncio.run(main())  # main loop
+asyncio.run(main(sio))  # main loop
